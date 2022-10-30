2a3da0331b2352e8ba32d2dbbf6a0398764f04f9
casim <mcasimdede@gmail.com>
2022-10-30 23:14:50 +0300

    added formater

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
 // import logo from './logo.svg';
-import './App.css';
-import Input from './components/input/Input';
-import Button from './components/button/Button';
-import Form from './components/form/Form';
-import { useState } from 'react';
+import "./App.css";
+import Input from "./components/input/Input";
+import Button from "./components/button/Button";
+import Form from "./components/form/Form";
+import { useState } from "react";
 function App() {
-
-  const[user,setUser]=useState({name:"",password:""});
-  const handleChange=(e)=>{
-    if(e.target.type==="text")
-    {setUser({...user, name:e.target.value});
-    console.log(user);
+  const [user, setUser] = useState({ name: "", password: "" });
+  const handleChange = (e) => {
+    if (e.target.type === "text") {
+      setUser({ ...user, name: e.target.value });
+      console.log(user);
     } else {
-      setUser({...user, password:e.target.value});
+      setUser({ ...user, password: e.target.value });
       console.log(user);
     }
-      
   };
-    
+
   return (
     <div className="App">
       <h2>Login Page</h2>
       <Form>
-        <Input type={"text"} value={"user.name"} placeHolder={"User name" } onChange={handleChange}></Input>
-        <Input type={"password"} value={"user.password"} onChange={handleChange} placeHolder="Password" />
-        <Button onClick={()=> alert("merhaba")}/>
+        <Input
+          type={"text"}
+          value={"user.name"}
+          placeHolder={"User name"}
+          onChange={handleChange}
+        ></Input>
+        <Input
+          type={"password"}
+          value={"user.password"}
+          onChange={handleChange}
+          placeHolder="Password"
+        />
+        <Button onClick={() => alert("Hello")} />
       </Form>
     </div>
   );
--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,10 +1,13 @@
-import React from "react"
-import "./input.css"
-function Input({type, placeHolder,onChange}){
-    return(
-    
-        <input className="input" type={type} placeholder={placeHolder} onChange={onChange} ></input>
-  
-    )
+import React from "react";
+import "./input.css";
+function Input({ type, placeHolder, onChange }) {
+  return (
+    <input
+      className="input"
+      type={type}
+      placeholder={placeHolder}
+      onChange={onChange}
+    ></input>
+  );
 }
 export default Input;
